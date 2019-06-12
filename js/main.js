c3cd8555d030d4af5e444d7bd64956840d52f4a0
MikhailMos <mos-mv@mail.ru>
2019-06-12 22:34:21 +0300

    Исправляет ошибки, выявленные наставником

--- js/main.js
+++ js/main.js
@@ -3,44 +3,45 @@
 var ARR_LENGTH = 8;
 var Y_MIN = 130;
 var Y_MAX = 630;
+var MAP_PIN_WIDTH = 50;
+var MAP_PIN_HEIGHT = 70;
+var PATH_TO_IMG = 'img/avatars/user';
 var TYPE_OF_PLACE = ['palace', 'flat', 'house', 'bungalo'];
 
-var arrMyObjects = [];
-var widthMapPins = document.querySelector('.map__pins').offsetWidth;
-var mapPinElementWidth = document.querySelector('.map__pin').offsetWidth;
-var mapPinElementHeight = document.querySelector('.map__pin').offsetHeight;
-var similarListElement = document.querySelector('.map__pins');
-var similarPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var pins = [];
+var map = document.querySelector('.map__pins');
+var widthMapPins = map.offsetWidth;
+var userPin = document.querySelector('#pin').content.querySelector('.map__pin');
 var fragment = document.createDocumentFragment();
 
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-var MyObject = function (index, address, type, x, y) {
-  this.author = {'avatar': address + '0' + index + '.png'};
-  this.offer = {'type': type};
-  this.location = {'x': x, 'y': y};
+var MyObject = function (index) {
+  this.author = {'avatar': PATH_TO_IMG + '0' + index + '.png'};
+  this.offer = {'type': TYPE_OF_PLACE[getRandomInt(0, TYPE_OF_PLACE.length)]};
+  this.location = {'x': getRandomInt(0, widthMapPins), 'y': getRandomInt(Y_MIN, Y_MAX)};
 };
 
-var renderMapPin = function (mapPin) {
-  var mapPinElement = similarPinTemplate.cloneNode(true);
-  var imgElement = mapPinElement.querySelector('img');
+var getTemplatePin = function (mapPin) {
+  var userPinElement = userPin.cloneNode(true);
+  var imgPin = userPinElement.querySelector('img');
 
+  userPinElement.style.left = String(mapPin.location.x - (MAP_PIN_WIDTH / 2)) + 'px';
+  userPinElement.style.top = String(mapPin.location.y - MAP_PIN_HEIGHT) + 'px';
 
-  mapPinElement.style.left = String(mapPin.location.x - (mapPinElementWidth / 2)) + 'px';
-  mapPinElement.style.top = String(mapPin.location.y - mapPinElementHeight) + 'px';
+  imgPin.src = mapPin.author.avatar;
+  imgPin.alt = 'Тут могла бы быть Ваша реклама';
 
-  imgElement.src = mapPin.author.avatar;
-  imgElement.alt = 'Тут могла бы быть Ваша реклама';
-
-  return mapPinElement;
+  return userPinElement;
 };
 
 for (var i = 1; i <= ARR_LENGTH; i++) {
-  arrMyObjects[i] = new MyObject(i, 'img/avatars/user', TYPE_OF_PLACE[getRandomInt(0, TYPE_OF_PLACE.length)], getRandomInt(0, widthMapPins), getRandomInt(Y_MIN, Y_MAX));
-  fragment.appendChild(renderMapPin(arrMyObjects[i]));
+  // pins[i - 1] = new MyObject(i);
+  pins.push(new MyObject(i));
+  fragment.appendChild(getTemplatePin(pins[i - 1]));
 }
-similarListElement.appendChild(fragment);
+map.appendChild(fragment);
 
 document.querySelector('.map').classList.remove('map--faded');
