afe9c6be1cfb193ab645887bbee1d3b38980e791
MikhailMos <mos-mv@mail.ru>
2019-06-30 22:00:49 +0300

    Изменяет название константы MAIN_PIN_WIDTH -> MAIN_PIN_HALF_WIDTH и поменяно его значение

--- js/const.js
+++ js/const.js
@@ -5,11 +5,11 @@
     ENTER_CODE: 13,
     MAP_Y_MIN: 130,
     MAP_Y_MAX: 630,
-    MAIN_PIN_WIDTH: 65,
+    MAIN_PIN_HALF_WIDTH: 32,
     MAIN_PIN_HEIGHT: 80,
     PATH_TO_IMG: 'img/avatars/user',
     TYPE_OF_PLACE: ['palace', 'flat', 'house', 'bungalo'],
-    MAP_PIN_WIDTH: 50,
+    MAP_PIN_HALF_WIDTH: 25,
     MAP_PIN_HEIGHT: 70,
     ARR_LENGTH: 8
   };
