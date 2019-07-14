e234b205f6e88fd129ef6cb048ed0f27904185c6
MikhailMos <mos-mv@mail.ru>
2019-07-14 20:34:47 +0300

    Убирает закомментированный код

--- js/backend.js
+++ js/backend.js
@@ -25,20 +25,6 @@
   };
 
   var load = function (onSuccess, onError) {
-    // var xhr = new XMLHttpRequest();
-    // xhr.responseType = 'json';
-
-    // xhr.addEventListener('load', function () {
-    //   if (xhr.status === STATUS_OK) {
-    //     onSuccess(xhr.response);
-    //   } else {
-    //     onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-    //   }
-    // });
-
-    // xhr.addEventListener('error', function () {
-    //   onError('Статус ответа: ошибка соединения');
-    // });
     var xhr = getXhr(onSuccess, onError);
 
     xhr.addEventListener('timeout', function () {
