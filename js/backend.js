8c81c8937f4c85be700cef2e567e4d653d745c45
MikhailMos <mos-mv@mail.ru>
2019-07-15 01:56:23 +0300

    Добавляет описания функциям JSDoc

--- js/backend.js
+++ js/backend.js
@@ -5,6 +5,13 @@
   var STATUS_OK = 200;
   var TIMEOUT_VALUE = 10000;
 
+  /**
+   * Возвращает объект к дальнейшему запросу к серверу
+   *
+   * @param {object} onSuccess - функция onSuccessHandler (при загрузке), onSaveHandler (при отправке)
+   * @param {string} onError - функция onErrorHandler
+   * @return {object}
+   */
   var getXhr = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
@@ -24,6 +31,12 @@
     return xhr;
   };
 
+  /**
+   * Загружает данные с сервера
+   *
+   * @param {object} onSuccess - функция onSuccessHandler
+   * @param {string} onError - функция onErrorHandler
+   */
   var load = function (onSuccess, onError) {
     var xhr = getXhr(onSuccess, onError);
 
@@ -37,6 +50,13 @@
     xhr.send();
   };
 
+  /**
+   * Отправляет данные формы на сервер
+   *
+   * @param {object} data - заполненная форма для отправки на сервер
+   * @param {object} onSuccess - функция onSaveHandler
+   * @param {string} onError - функция onErrorHandler
+   */
   var upload = function (data, onSuccess, onError) {
     var xhr = getXhr(onSuccess, onError);
 
