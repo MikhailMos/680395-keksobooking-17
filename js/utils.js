d579fa587155f8fdcd78b4b4c195ceef4a13f718
MikhailMos <mos-mv@mail.ru>
2019-07-15 02:47:50 +0300

    Добавляет функции изменения статуса

--- js/utils.js
+++ js/utils.js
@@ -11,42 +11,57 @@
     item.disabled = window.utils.isActive !== true;
   };
 
-  window.utils = {
-    isActive: false,
-
-    /**
-     * Получаем случайное целое число
-     * ф-я экспортная
-     *
-     * @param {number} min - начальное значение
-     * @param {number} max - конечное значение
-     * @return {number}
-     */
-    getRandomInt: function (min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    },
-
-    /**
-     * Перебираем массив и устанавливаем значение свойству, каждого элемента
-     * ф-я экспортная
-     *
-     * @param {array} arr
-     */
-    enumeratesArray: function (arr) {
-      arr.forEach(getProperty);
-    },
-
-    /**
-     * Отлавнивает нажатие на кнопку энтер
-     * ф-я экспортная
-     *
-     * @param {object} evt
-     * @param {function} action - импортируемая функция
-     */
-    isEnterEvent: function (evt, action) {
-      if (evt.keyCode === window.const.keyCode.ENTER) {
-        action();
-      }
+  /**
+   * Отлавнивает нажатие на кнопку энтер
+   * ф-я экспортная
+   *
+   * @param {object} evt
+   * @param {function} action - импортируемая функция
+   */
+  var isEnterEvent = function (evt, action) {
+    if (evt.keyCode === window.const.keyCode.ENTER) {
+      action();
     }
+  };
+
+  /**
+   * Перебираем массив и устанавливаем значение свойству, каждого элемента
+   * ф-я экспортная
+   *
+   * @param {array} arr
+   */
+  var enumeratesArray = function (arr) {
+    arr.forEach(getProperty);
+  };
+
+  /**
+   * Получаем случайное целое число
+   * ф-я экспортная
+   *
+   * @param {number} min - начальное значение
+   * @param {number} max - конечное значение
+   * @return {number}
+   */
+  var getRandomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  var isActiveEnabled = function () {
+    window.utils.isActive = true;
+  };
+
+  var isActiveDisabled = function () {
+    window.utils.isActive = false;
+  };
+
+  var isActive = false;
+
+  window.utils = {
+    isActive: isActive,
+    isActiveEnabled: isActiveEnabled,
+    isActiveDisabled: isActiveDisabled,
+    getRandomInt: getRandomInt,
+    enumeratesArray: enumeratesArray,
+    isEnterEvent: isEnterEvent
   };
 })();
