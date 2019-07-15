3008e4953ddfe07eba764f95737686ae7943507a
MikhailMos <mos-mv@mail.ru>
2019-07-15 20:22:40 +0300

    Убирает ф-ю getRandomInt

--- js/utils.js
+++ js/utils.js
@@ -12,8 +12,7 @@
   };
 
   /**
-   * Отлавнивает нажатие на кнопку энтер
-   * ф-я экспортная
+   * Отлавнивает нажатие на кнопку Enter
    *
    * @param {object} evt
    * @param {function} action - импортируемая функция
@@ -25,25 +24,24 @@
   };
 
   /**
+   * Отлавнивает нажатие на кнопку ESC
+   *
+   * @param {object} evt
+   * @param {function} action - импортируемая функция
+   */
+  var isESCEvent = function (evt, action) {
+    if (evt.keyCode === window.const.keyCode.ESC) {
+      action();
+    }
+  };
+
+  /**
    * Перебираем массив и устанавливаем значение свойству, каждого элемента
-   * ф-я экспортная
    *
    * @param {array} arr
    */
   var enumeratesArray = function (arr) {
     arr.forEach(getProperty);
-  };
-
-  /**
-   * Получаем случайное целое число
-   * ф-я экспортная
-   *
-   * @param {number} min - начальное значение
-   * @param {number} max - конечное значение
-   * @return {number}
-   */
-  var getRandomInt = function (min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
   };
 
   var isActiveEnabled = function () {
@@ -60,8 +58,8 @@
     isActive: isActive,
     isActiveEnabled: isActiveEnabled,
     isActiveDisabled: isActiveDisabled,
-    getRandomInt: getRandomInt,
     enumeratesArray: enumeratesArray,
-    isEnterEvent: isEnterEvent
+    isEnterEvent: isEnterEvent,
+    isESCEvent: isESCEvent
   };
 })();
